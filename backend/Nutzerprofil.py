d393c8a6b0bd7457a8cae26d9ad26e908c017234
inf3205 <inf3205@hs-worms.de>
2023-11-14 15:24:21 +0100

    change Startseite

--- backend/Nutzerprofil.py
+++ backend/Nutzerprofil.py
@@ -1,17 +1,18 @@
 import os.path
+import sys
 
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-class ProfileWindow(QMainWindow):
+class UserprofileWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super().__init__() # vereinfacht das Erstellen weiterer Subklassen
         uic.loadUi(os.path.join("..", "frontend", "Nutzerprofil.ui"), self)
         self.show()
 
-def main():
-    app = QApplication([])
-    profileWindow = ProfileWindow()
-    app.exec_()
-
-main()
+# if main program, run app, otherwise just import class
+if __name__ == "__main__":
+    app = QApplication(sys.argv) # construct QApp before QWidget
+    window = UserprofileWindow()
+    window.show()  # class Mainwindow aufrufen
+    sys.exit(app.exec_()) # exit cleanly
--- backend/Startseite.py
+++ backend/Startseite.py
@@ -1,58 +1,51 @@
+import os.path
 import sys
+
 from PyQt5.QtWidgets import *
-from frontend.Startseite_ui import Ui_MainWindow
+from PyQt5 import uic
 
-
-class Mainwindow:
+class StartpageWindow(QMainWindow):  
     def __init__(self):
-        self.main_win = QMainWindow()
-        self.uic = Ui_MainWindow()
-        self.uic.setupUi(self.main_win)
+        super().__init__() # vereinfacht das Erstellen weiterer Subklassen
+        uic.loadUi(os.path.join("..", "frontend", "Startseite.ui"), self)
+        self.show()
+          
+        # fuegen Typ von Traktor hinzu
+        self.typ_comboBox.addItem("Traktor 1")
+        self.typ_comboBox.addItem("Traktor 2")
+        self.typ_comboBox.addItem("Traktor 3")
+        self.typ_comboBox.addItem("Traktor 4")
 
-        # ### fugen Typ von Traktor hinzu
+        # Button deklarieren
+        self.bufferleer_button.clicked.connect(self.empty_search_info)
+        self.such_infor_commit.clicked.connect(self.confirm_search_info)
 
-        self.uic.typ_comboBox.addItem("Traktor 1")
-        self.uic.typ_comboBox.addItem("Traktor 2")
-        self.uic.typ_comboBox.addItem("Traktor 3")
-        self.uic.typ_comboBox.addItem("Traktor 4")
+        # self.lineEdit.editingFinished()         # press "enter" to finish
+        # self.lineEdit.returnPressed()           # return content if you press "enter"
 
-        # ## Button deklarieren
+    # loeschen alle Eintraege, in den User Suchinformation drauf schreiben
+    def empty_search_info(self):
+        self.katergorie_lineedit.clear()
+        self.zustand_lineedit.clear()
+        self.baujahr_lineEdit.clear()
+        self.leistung_linedit.clear()
+        self.km_lineedit.clear()
+        self.hersteller_lineedit.clear()
+        self.typ_comboBox.clear()
 
-        self.uic.bufferleer_button.clicked.connect(self.such_infor_leermachen)
-        self.uic.such_infor_commit.clicked.connect(self.such_infor_bestatigen)
-
-        # self.uic.lineEdit.editingFinished()         # press "enter" to finish
-        # self.uic.lineEdit.returnPressed()           # return content if you press "enter"
-
-    # ### loschen alle Eintragen, in den User Suchinformation drauf schreiben
-    def such_infor_leermachen(self):
-        self.uic.katergorie_lineedit.clear()
-        self.uic.zustand_lineedit.clear()
-        self.uic.baujahr_lineEdit.clear()
-        self.uic.leistung_linedit.clear()
-        self.uic.km_lineedit.clear()
-        self.uic.hersteller_lineedit.clear()
-        self.uic.typ_comboBox.clear()
-
-    # ### hol mal alle Informationen, die User bereits in Eintragen eingetragen
-
-    def such_infor_bestatigen(self):
-
+    # hol mal alle Informationen, die User bereits eingetragen hat
+    def confirm_search_info(self):
         print("Button click!")
-        print("Kategorie: ", self.uic.katergorie_lineedit.text())
-        print("Zustand: ", self.uic.zustand_lineedit.text())
-        print("Leistung: ", self.uic.leistung_linedit.text())
-        print("Km/h: ", self.uic.km_lineedit.text())
-        print("Hersteller: ", self.uic.hersteller_lineedit.text())
-        print("Typ: ", self.uic.typ_comboBox.currentText())
+        print("Kategorie: ", self.katergorie_lineedit.text())
+        print("Zustand: ", self.zustand_lineedit.text())
+        print("Leistung: ", self.leistung_linedit.text())
+        print("Km/h: ", self.km_lineedit.text())
+        print("Hersteller: ", self.hersteller_lineedit.text())
+        print("Typ: ", self.typ_comboBox.currentText())
 
-    def show(self):
-        self.main_win.show()
-
-
+# if main program, run app, otherwise just import class
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    main_win = Mainwindow()  # class Mainwindow aufrufen
-    main_win.show()
-
-    sys.exit(app.exec_())
+    app = QApplication(sys.argv) # construct QApp before QWidget
+    window = StartpageWindow()
+    window.show()  # class Mainwindow aufrufen
+    sys.exit(app.exec_()) # exit cleanly
