5746ab88c24ec4262ecb4b3c31462487920d9806
inf3205 <inf3205@hs-worms.de>
2023-12-03 19:21:21 +0100

    - added function to getSumme of Products   - and whether the User can afford. - removed "." from csv Produktpreisen to fix reading error

--- backend/getRandoms.py
+++ backend/getRandoms.py
@@ -1,5 +1,9 @@
 import random
 import csv
+import os.path
+# import dis # show byte code with dis.dis(<function>)
+
+CSV_PATH = os.path.join("..", "resources", "csv")
 
 # Zufallszahl 1
 def isInterested(threshold = 0.6):
@@ -20,26 +24,65 @@ def increasedBudget(curBudget, years = 1):
         newBudget += increase
     return newBudget
 
-def calcSumme(modellnummer, geraeteTyp, anzahl, accountname):
+def getSumme(geraeteArt, geraeteTyp, anzahl, account):
     
-    if geraeteTyp == "Trak":
-        pass
-        
-    if geraeteTyp == "Zube":
-        pass
-        
-    with open('some.csv', newline='') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            print(row)
-        
-    with open('some.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows() # insert here someiterable
+    traktorenFile = os.path.join(CSV_PATH, r"mobile Arbeitsmaschinen Landwirtschaft.csv")
+    zubehoerFile = os.path.join(CSV_PATH, r"Zubehör.csv")
+    accountsFile = os.path.join(CSV_PATH, r"Accounts.csv")
     
-    # budget cheken, ist genug da?
+    def getGeraeteSumme():  
+        if geraeteArt == "Traktor":
+            with open(traktorenFile, newline='') as file:
+                for row in csv.reader(file):
+                    if geraeteTyp in row:
+                        preis = int(row[4])
+                        summe = anzahl * preis 
+                        if anzahl <= int(row[6]): # auf Lager
+                            return summe
+                        else:
+                            return False
+                return False      
+        elif geraeteArt == "Zubehör":
+            with open(zubehoerFile, newline='') as file:
+                for row in csv.reader(file):
+                    if geraeteTyp in row:
+                        preis = int(row[1])
+                        summe = anzahl * preis
+                        if anzahl <= int(row[2]): # Bestand
+                            return summe
+                        else:
+                            return False
+                return False
+                    
+    def accountCanAfford(summe):
+        with open(accountsFile, newline='') as file:
+            for row in csv.reader(file):
+                if account in row:
+                    if summe <= int(row[2]): # Budget
+                        return "can afford"
+                    else:
+                        return "can not afford"
+            return "account not found"
+        
+    result = getGeraeteSumme()
+    summe = result if result else False
+    canAff = accountCanAfford(summe) if summe else False
+    return summe, canAff
+    
+# print(getSumme(geraeteArt = "Traktor", 
+#                   geraeteTyp = "Profi_6000", 
+#                   anzahl = 5,
+#                   account= "Klaus"))
+       
+    # budget checken, ist genug da?
     # lagerbestand checken, genug da?
     # lagerbestand abziehen
+    # return summe
+        
+        
+    # with open('some.csv', 'w', newline='') as file:
+        # writer = csv.writer(file)
+        # writer.writerows() # insert here someiterable
     
-    return # summe
+    
 
